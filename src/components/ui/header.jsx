a907bb30a362affebc25d989b218b3247e1f06aa
rohitsharma <rs0043071@gmail.com>
2025-01-24 22:14:07 +0530

    vercel

--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -6,7 +6,9 @@ const Header = () => {
             <Sidebar />
 
             <div className="px-4 p-2 flex gap-5">
-                <Button variant='outline' className="rounded-2xl hover:shadow-[0_0_30px_0_#64FFDA80] font-[wlpsgx] text-2xl">Resume</Button>
+                <Button variant='outline' className="rounded-2xl hover:shadow-[0_0_30px_0_#64FFDA80] font-[wlpsgx] text-2xl"
+
+                ><a href="file:///C:/Users/rs004/Downloads/Untitled%20document.pdf">Resume</a></Button>
                 <a href="https://github.com/rohit-sharma12"><img className="w-8" src="icons/Github.png" alt="" /></a>
                 <a href="https://www.linkedin.com/in/rohit-sharma-917017309/"><img className="w-8" src="/public/linkedin.png" alt="" /></a>
                 <a href="mailto:rs0043071@gmail.com"><img className="w-8" src="/src/assets/communication.png" alt="" /></a>
