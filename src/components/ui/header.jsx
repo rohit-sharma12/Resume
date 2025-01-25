04272d4c7a784aeccc4017304e1481227d60528f
rohitsharma <rs0043071@gmail.com>
2025-01-25 22:39:36 +0530

    commit

--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -9,9 +9,9 @@ const Header = () => {
                 <Button variant='outline' className="rounded-2xl hover:shadow-[0_0_30px_0_#64FFDA80] font-[wlpsgx] text-2xl"
 
                 ><a href="file:///C:/Users/rs004/Downloads/Untitled%20document.pdf">Resume</a></Button>
-                <a href="https://github.com/rohit-sharma12"><img className="w-8" src="public/icons/Github.png" alt="" /></a>
-                <a href="https://www.linkedin.com/in/rohit-sharma-917017309/"><img className="w-8" src="public/icons/linkedin.png" alt="" /></a>
-                <a href="mailto:rs0043071@gmail.com"><img className="w-8" src="public/icons/communication.png" alt="" /></a>
+                <a href="https://github.com/rohit-sharma12"><img className="w-8" src="/icons/Github.png" alt="" /></a>
+                <a href="https://www.linkedin.com/in/rohit-sharma-917017309/"><img className="w-8" src="/icons/linkedin.png" alt="" /></a>
+                <a href="mailto:rs0043071@gmail.com"><img className="w-8" src="/icons/communication.png" alt="" /></a>
             </div>
 
         </div>
